765e67b70325cec1d2d8645598d3addf0cc03c85
Ingrid Aquino <ingridaquinosoares@live.com>
2022-07-20 11:20:34 -0300

    :construction: Created router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./styles/themes/global";
-import { Button } from "./Components/Button";
-import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
+
+import { GlobalStyle } from './styles/themes/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-
   return (
-      <ThemeProvider theme={defaultTheme}>
-        <Button variant="primary"/>
-        <Button variant="secondary"/>
-        <Button variant="danger"/>
-        <Button variant="success"/>
-
-
-        <GlobalStyle />
-      </ThemeProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
